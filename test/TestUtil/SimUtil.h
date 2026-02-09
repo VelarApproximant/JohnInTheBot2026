b2faea29e9bd366df467212f0c900798e02ec6ee
Kayvon devonte williams 🥷🥷🥷 <145631119+VelarApproximant@users.noreply.github.com>
2026-02-08 20:23:16 -0800

    clean SimUtil

--- test/TestUtil/SimUtil.h
+++ test/TestUtil/SimUtil.h
@@ -1,5 +1,3 @@
-// SimUtil.h
-#pragma once
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -8,12 +6,12 @@
 
 using String = std::string;
 
-// ---- Arduino-like helpers ----
+// simulating arduino functions in c++
 inline void delay(int ms) {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-inline int constrain(int val, int minVal, int maxVal) { //a clamp; takes a value and clamps it between 2nd and 3rd param
+inline int constrain(int val, int minVal, int maxVal) { 
     if (val < minVal) {
         return minVal;
     }
@@ -24,22 +22,21 @@ inline int constrain(int val, int minVal, int maxVal) { //a clamp; takes a value
 }
 
 inline int map(int x, int in_min, int in_max, int out_min, int out_max) { //replicating arduino's map function
-    return (x - in_min) * (out_max - out_min) /
-           (in_max - in_min) + out_min;
+    int val = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return val;
 }
 
-// simulation for a serial bus as the sim is just c++ without any dev board
-class HardwareSerial { //creates new class called HardwareSerial
+class HardwareSerial {
 public:
-    HardwareSerial() {} //empty default constructor (this is automatically generated but I added one for expolaining)
+    HardwareSerial() {}
 
-    void begin(int baud) { //mimics how you would initialize an actual ESP board
+    void begin(int baud) { 
         std::cout << "serial begin at " << baud << " baud\n"; //legit just prints that it begins
     }
 
     void write(uint8_t* data, size_t length) { //length should almost always be 4
-        std::cout << "uart Packet: "; //print out "uart Packet"
-        for (size_t i = 0; i < length; i++) { //loops through each byte and prints it in hexadecimal
+        std::cout << "uart Packet: "; 
+        for (size_t i = 0; i < length; i++) { 
             std::cout << std::hex << (int)data[i];
         }
         std::cout << std::dec; //switches the hexadecimal value back to normal deciaml numbers
